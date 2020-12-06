5718a1da14e73f8014d8f33b33dd9bf362a9db40
Tomáš Lessy <tomas.lessy@firma.seznam.cz>
2020-12-06 08:52:43 +0100

    Day 6

---
+++ Day_6/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+trans = str.maketrans('\n', ' ')
+
+with open('input', 'r') as fd:
+    data = [line for line in fd]
+
+data = ''.join(data).split('\n\n')
+data = [g.translate(trans).strip().split() for g in data]
+
+
+def person_choices(d, operation):
+    sets = []
+    for g in data:
+        s = set(g[0])
+        for p in g:
+            s = operation(s, set(p))
+
+        sets.append(s)
+    return sets
+
+
+sets = person_choices(data, lambda s1, s2: s1.union(s2))
+print('Part1 -> ', sum([len(s) for s in sets]))
+
+sets = person_choices(data, lambda s1, s2: s1.intersection(s2))
+print('Part 2 -> ', sum([len(s) for s in sets]))
